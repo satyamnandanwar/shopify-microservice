7c0ddfa3edc3f3b5e85defe079051c9d9f3e2bfa
Satyam Nandanwar <samnand.edu@gmail.com>
2026-03-27 02:13:03 +0530

    Initial clean commit - product service

---
+++ product-service/src/test/java/com/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.REMOVED.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
