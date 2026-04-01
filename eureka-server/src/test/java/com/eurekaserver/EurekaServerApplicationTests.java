a619645cda922e09c07441679833997d4968f75e
Satyam Nandanwar <samnand.edu@gmail.com>
2026-04-01 16:40:48 +0530

    Added Eureka Server module

---
+++ eureka-server/src/test/java/com/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.REMOVED.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
